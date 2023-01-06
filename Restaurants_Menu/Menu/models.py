84b6ffbbb1fa53bd5cb8603f7a33a4d69bc96ad3
mosafari <m.safari2680@gmail.com>
2023-01-06 13:32:27 +0330

    create User model

--- Restaurants_Menu/Menu/models.py
+++ Restaurants_Menu/Menu/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+
+    use_in_migration = True
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is Required')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+
+    username = None
+    name = models.CharField(max_length=100, unique=True)
+    restaurant =models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    objects = UserManager()
+
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []#'name']
+
+    def __str__(self):
+        return self.name
+    
