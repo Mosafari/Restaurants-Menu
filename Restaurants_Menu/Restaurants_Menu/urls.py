04f360822c266c4e91ea664b67b6b6b0870be821
mosafari <m.safari2680@gmail.com>
2023-01-06 12:05:29 +0330

    adding paths

--- Restaurants_Menu/Restaurants_Menu/urls.py
+++ Restaurants_Menu/Restaurants_Menu/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('Menu.urls')),
+    # adding restaurant path for auth 
+    path("restaurant/", include("django.contrib.auth.urls")),
 ]
