306c8f8f27637b81747b1aa78124522f6bc5c7e5
mosafari <m.safari2680@gmail.com>
2023-01-07 12:25:02 +0330

    adding signup view

--- Restaurants_Menu/Menu/urls.py
+++ Restaurants_Menu/Menu/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import SignUpView
 
 # adding paths
 urlpatterns = [
-    path('signup/', views),
-    path('menus/', views),
+    path('signup/', SignUpView.as_view(), name="signup"),
+
     
 ]
--- Restaurants_Menu/Menu/views.py
+++ Restaurants_Menu/Menu/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
+from django.urls import reverse_lazy
+from django.views import View
+
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            return HttpResponse(render(request, 'signup.html'), 200)
+        
+
+        
