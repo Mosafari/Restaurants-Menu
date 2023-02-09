154708f9ea811f1ea34e0d0ee86c49529b9282b5
mosafari <m.safari2680@gmail.com>
2023-02-09 12:31:46 +0330

    adding img field to menu models

--- Restaurants_Menu/Restaurants_Menu/urls.py
+++ Restaurants_Menu/Restaurants_Menu/urls.py
@@ -20,3 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('Menu.urls')),
 ]
+
+from django.conf import settings  
+from django.conf.urls.static import static  
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
